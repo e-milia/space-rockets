700e5b84393ae208f191157750df63d3d15c892f
Emilia Ponomareva <es.ponomareva@gmail.com>
2020-10-31 16:41:47 +0000

    SPACE4: add launchPad tests

---
+++ test/data/mock-launchpad-data.js
@@ -0,0 +1,18 @@
+export default {
+  data: {
+    id: 4,
+    name: 'KSC LC 39A',
+    status: 'active',
+    location: {
+      name: 'Cape Canaveral', region: 'Florida', latitude: 28.6080585, longitude: -80.6039558,
+    },
+    vehicles_launched: ['Falcon 9', 'Falcon Heavy'],
+    attempted_launches: 26,
+    successful_launches: 25,
+    wikipedia: 'https://en.wikipedia.org/wiki/Kennedy_Space_Center_Launch_Complex_39#Launch_Complex_39A',
+    details: 'NASA historic launch pad that launched most of the Saturn V and Space Shuttle missions. Initially for Falcon Heavy launches, it is now launching all of SpaceX east coast missions due to the damage from the AMOS-6 anomaly. After SLC-40 repairs are complete, it will be upgraded to support Falcon Heavy, a process which will take about two months. In the future it will launch commercial crew missions and the Interplanetary Transport System.',
+    site_id: 'ksc_lc_39a',
+    site_name_long: 'Kennedy Space Center Historic Launch Complex 39A',
+  },
+  error: null,
+};
